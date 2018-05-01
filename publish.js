eca2b8600570c2d9ab320389d31410c064a2889f
Rudolf Halas <halasrudo@gmail.com>
2018-05-01 11:49:37 +0200

    chore: Added travis and a publishing script

---
+++ publish.js
@@ -0,0 +1,22 @@
+const axios = require('axios')
+
+const siteName = 'halas.codes'
+const authToken = process.env.NETLIFY_ACCESS_TOKEN;
+
+(async function () {
+  try {
+    let options = { headers: { Authorization: `Bearer ${authToken}` } }
+
+    let res = await axios.get(`https://api.netlify.com/api/v1/sites/${siteName}/deploys`, options)
+
+    const latestDeployId = res.data[0].id
+
+    console.log(`Publishing build ${latestDeployId}`)
+
+    res = await axios.post(`https://api.netlify.com/api/v1/sites/${siteName}/deploys/${latestDeployId}/restore`, {}, options)
+
+    console.log(res.statusText)
+  } catch (err) {
+    console.error('Unable to publish the release: ' + err.message)
+  }
+})()
