846e80f4d684b83a371d72925a6049c6688aa005
Rudolf Halas <halasrudo@gmail.com>
2018-05-15 00:36:27 +0200

    feat: Added analytics

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
 import './styles/compiled/app.css'
 import App from './pages/App'
 import registerServiceWorker from './bootstrap/registerServiceWorker'
+
+ReactGA.initialize('UA-36731267-7')
 
 ReactDOM.render(<App/>, document.getElementById('root'))
 registerServiceWorker()
